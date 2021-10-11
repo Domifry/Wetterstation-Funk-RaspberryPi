66c4b5de5ecf2fe33b07a3f9bc4c342a7da55079
Dominic <lindner.dominic@gmail.com>
2021-10-11 18:16:52 +0200

    Update rtl-sql.py

--- rtl-sql.py
+++ rtl-sql.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-# Github Domifry
-# Support Github mrkwenzel
+
 
 import mysql.connector
 import time
@@ -9,50 +8,75 @@ import os
 import sys
 import http.client, urllib
 
-proc = subprocess.Popen(['rtl_433','-R', '73', '-F', 'json', '-f', '433.9M'], stdout=subprocess.PIPE)
+proc = subprocess.Popen(['rtl_433','-R', '73', '-R','37','-F', 'json'], stdout=subprocess.PIPE)
 sensor1 = False
 sensor2 = False
 sensor3 = False
-sensor4 = False
 connected = False
 counter = 0
 
 while True:
-  line = proc.stdout.readline())
+  line = proc.stdout.readline()
+  #print(line.rstrip())
   json1 = line.rstrip()
   json = str(json1)
   json = json.lstrip("'")
   json = json.lstrip("b")
   json = json.lstrip("'")
   json = json.rstrip("'")
+  #print("geht:"+json)
   counter+=1
+  #if not line:
+  #  break
   if connected == False:
-   connection = mysql.connector.connect(host="ID", user="user", passwd="pass", db="user")
+   connection = mysql.connector.connect(host="85.13.145.112", user="d03794ba", passwd="uP6TVnAT4JH2FXe3", db="d03794ba")
    cursor = connection.cursor()
    connected = True
-  if "ID" in json and sensor1 == False:
-   statement = "INSERT INTO `NAME`.`Schatten` (`ID`, `Schatten`, `Zeit`) VALUES (NULL,' "+json+"', current_timestamp())"
+  if "226" in json and sensor1 == False:
+   statement = "INSERT INTO `d03794ba`.`Schatten` (`ID`, `Schatten`, `Zeit`) VALUES (NULL,' "+json+"', current_timestamp())"
    cursor.execute(statement)
    sensor1 = True
-  if "ID" in json and sensor2 == False:
-   statement = "INSERT INTO `NAME`.`Sonne` (`ID`, `Sonne`, `Zeit`) VALUES (NULL, '"+json+"', current_timestamp())"
+  if "240" in json and sensor2 == False:
+   statement = "INSERT INTO `d03794ba`.`Sonne` (`ID`, `Sonne`, `Zeit`) VALUES (NULL, '"+json+"', current_timestamp())"
    cursor.execute(statement)
    sensor2 = True
-  if "ID" in json and sensor3 == False and "wind_avg_km_h" in json:
-   statement = "INSERT INTO `NAME`.`Wind` (`ID`, `Wind`, `Zeit`) VALUES (NULL,'"+json+"', current_timestamp())"
+  if "86798" in json and sensor3 == False and "wind_avg_km_h" in json:
+   statement = "INSERT INTO `d03794ba`.`Wind` (`ID`, `Wind`, `Zeit`) VALUES (NULL,'"+json+"', current_timestamp())"
    cursor.execute(statement)
    sensor3 = True
-  if "ID" in json and sensor4 == False and "rain" in json:
-   statement = "INSERT INTO `NAME`.`Regen` (`ID`, `Wind`, `Zeit`) VALUES (NULL,'"+json+"', current_timestamp())"
+  if "Inovalley-kw9015b" in json and sensor4 == False:
+   statement = "INSERT INTO `d03794ba`.`Regen` (`ID`, `Regen`, `Zeit`) VALUES (NULL,'"+json+"', current_timestamp())"
    cursor.execute(statement)
-   sensor3 = True
+   sensor4 = True
   if counter == 100:
-   # sollte 100 mal nichts ankommen, startet er das script neu
    proc = subprocess.Popen(['rtl_433','-R', '73', '-F', 'json'], stdout=subprocess.PIPE)
+   nachricht = "100 erfolglose Versuche"
+   if sensor1 == False:
+    nachricht = nachricht+"<br>Sensor Schatten"
+   if sensor2 == False:
+    nachricht = nachricht+"<br>Sensor Sonne"
+   if sensor3 == False:
+    nachricht = nachricht+"<br>Sensor Wind"
+   if sensor4 == False:
+    nachricht = nachricht+"<br>Sensor Regen"
+   conn = http.client.HTTPSConnection("api.pushover.net:443")
+   conn.request("POST", "/1/messages.json",
+   urllib.parse.urlencode({
+   "token": "aengikvws6xiojofjw3qvwqj86cxca",
+   "user": "ur3teosv7vc23fyskfa4g643h8m4v1",
+   "message": nachricht,
+   }), { "Content-type": "application/x-www-form-urlencoded" })
+   conn.getresponse()
   if counter == 150:
-   #script ist kaputt und bricht ab
+   conn = http.client.HTTPSConnection("api.pushover.net:443")
+   conn.request("POST", "/1/messages.json",
+   urllib.parse.urlencode({
+   "token": "aengikvws6xiojofjw3qvwqj86cxca",
+   "user": "ur3teosv7vc23fyskfa4g643h8m4v1",
+   "message": "Wetterstation wird beendet!",
+   }), { "Content-type": "application/x-www-form-urlencoded" })
    break
-  if sensor1 == True and sensor2 == True and sensor3 == True and sensor4 == true:
+  if sensor1 == True and sensor2 == True and sensor3 == True:
    sensor1 = False
    sensor2 = False
    sensor3 = False
@@ -61,4 +85,9 @@ while True:
    connection.commit()
    cursor.close()
    counter = 0
+   #print("erfolg")
+   #time.sleep(10)
    time.sleep(900)
+   #print(line.rstrip())
+
+
